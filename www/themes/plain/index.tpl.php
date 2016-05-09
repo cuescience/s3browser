4d202ba230616f3e10603445f4bd8c7030534679
fhanspach <f.hanspach@cuescience.de>
2016-05-09 13:12:55 +0200

    added iphone meta tags

--- www/themes/plain/index.tpl.php
+++ www/themes/plain/index.tpl.php
@@ -3,6 +3,7 @@
    "http://www.w3.org/TR/html4/strict.dtd">
 
 <html lang="en">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="//bootswatch.com/cosmo/bootstrap.min.css"/>
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">
 <head>
